8b718950a71535c135b81d7076c0370a367c33f8
sonali350 <sonalimalviya736@gmail.com>
2024-09-11 23:30:35 +0530

    Add files via upload

---
+++ _7Remove_LastEle.java
@@ -0,0 +1,8 @@
+public class _7Remove_LastEle {
+    public static void main(String[] args) {
+        int arr[] = {2,3,4,5};
+        for(int i=0;i<arr.length-1;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
