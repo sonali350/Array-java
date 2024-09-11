1730b90cfccea9c148de241c57f32268be530ed2
sonali350 <sonalimalviya736@gmail.com>
2024-09-11 23:31:25 +0530

    Add files via upload

---
+++ _9Remove_First_Ele.java
@@ -0,0 +1,15 @@
+
+//9Remove the first element of the array [11, 12, 13, 14]
+
+class _9Remove_FirstEle{
+
+
+    public static void main(String[] args) {
+        int arr[] = {11, 12, 13, 14};
+       
+        for(int i=1;i<arr.length;i++){
+
+        System.out.println(arr[i]);
+        }
+    }
+}
